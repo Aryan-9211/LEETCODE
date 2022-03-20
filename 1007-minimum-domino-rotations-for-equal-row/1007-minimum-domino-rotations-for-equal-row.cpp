d89276bf92a6ce1ac32209624362248050e7ec3a
Aryan-9211 <71491341+Aryan-9211@users.noreply.github.com>
2022-03-20 14:01:20 +0530

    Time: 184 ms (52.64%), Space: 111.6 MB (72.27%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
@@ -4,36 +4,43 @@ class Solution
         int minDominoRotations(vector<int> &tops, vector<int> &bottoms)
         {
             int arr[7] = { 0 };
-            for (int i = 0; i < tops.size(); i++) arr[tops[i]]++;
-            for (int i = 0; i < bottoms.size(); i++) arr[bottoms[i]]++;
+            int n = tops.size();
+            for (int i = 0; i < n; i++)
+            {
+                arr[tops[i]]++;
+                arr[bottoms[i]]++;
+            }
             int max = 0;
-            int num = 0;
             for (int i = 1; i <= 6; i++)
             {
-                if (arr[i] > max)
+                if (arr[i] > arr[max])
                 {
-                    max = arr[i];
-                    num = i;
+                    max = i;
                 }
             }
             int count1 = 0;
             int x = 0;
-            for (int i = 0; i < tops.size(); i++)
+            for (int i = 0; i < n; i++)
             {
-                if (tops[i] == num) continue;
-                if (bottoms[i] == num) count1++;
+                if (tops[i] == max)
+                    continue;
+                if (bottoms[i] == max)
+                    count1++;
                 else
                 {
                     x = 1;
                     break;
                 }
             }
-            if (x) return -1;
+            if (x)
+                return -1;
             int count2 = 0;
-            for (int i = 0; i < tops.size(); i++)
+            for (int i = 0; i < n; i++)
             {
-                if (bottoms[i] == num) continue;
-                if (tops[i] == num) count2++;
+                if (bottoms[i] == max)
+                    continue;
+                if (tops[i] == max)
+                    count2++;
             }
             int m = min(count1, count2);
             return m;
