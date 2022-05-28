e7d0ae9e46c3662f68f0775d5e9b466bba088d9c
Aryan-9211 <71491341+Aryan-9211@users.noreply.github.com>
2022-05-28 11:23:17 +0530

    Time: 102 ms (9.72%), Space: 18.8 MB (94.20%) - LeetHub

---
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -0,0 +1,36 @@
+class Solution
+{
+    public:
+
+        bool check(vector<int> &v, int &h, int &x)
+        {
+            int count = 0;
+            for (int i = 0; i < v.size(); i++)
+            {
+                count += v[i] / x;
+                if (v[i] % x != 0) count++;
+            }
+            return count <= h;
+        }
+
+    int minEatingSpeed(vector<int> &v, int h)
+    {
+        int lo = 1, hi = 1e9, mid;
+        while (hi - lo > 1)
+        {
+            mid = lo + (hi - lo) / 2;
+            // cout << lo << " " << hi << " " << mid << endl;
+            if (check(v, h, mid))
+            {
+                hi = mid;
+            }
+            else
+            {
+                lo = mid + 1;
+            }
+        }
+        if (check(v, h, lo)) return lo;
+        if (check(v, h, hi)) return hi;
+        return 0;
+    }
+};
