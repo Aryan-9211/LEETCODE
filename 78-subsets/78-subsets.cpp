612e0ff60c0f1c8c41e48a41718e00cabb996284
Aryan-9211 <71491341+Aryan-9211@users.noreply.github.com>
2022-08-08 22:53:45 +0530

    Time: 11 ms (14.25%), Space: 7.2 MB (57.04%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -4,20 +4,17 @@ class Solution
         vector<vector < int>> subsets(vector<int> &nums)
         {
             int n = nums.size();
-            int subset = (1 << n);
-            vector<vector < int>> ans;
-            for (int i = 0; i < subset; i++)
+            int subset_size = 1 << n;
+            vector<vector < int>> subsets;
+            for (int i = 0; i < subset_size; i++)
             {
-                vector<int> help;
+                vector<int> subset;
                 for (int j = 0; j < n; j++)
                 {
-                    if (i &(1 << j))
-                    {
-                        help.push_back(nums[j]);
-                    }
+                    if (i &(1 << j)) subset.push_back(nums[j]);
                 }
-                ans.push_back(help);
+                subsets.push_back(subset);
             }
-            return ans;
+            return subsets;
         }
 };
