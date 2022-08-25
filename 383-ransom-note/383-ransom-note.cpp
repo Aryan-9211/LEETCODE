47f4dae063b31846e1380e6c3a39623002c7c865
Aryan-9211 <71491341+Aryan-9211@users.noreply.github.com>
2022-08-25 21:44:04 +0530

    Time: 45 ms (19.73%), Space: 9 MB (15.41%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -3,7 +3,7 @@ class Solution
     public:
         bool canConstruct(string ransomNote, string magazine)
         {
-            unordered_map<char, int> m;
+            map<char, int> m;
             for (auto i: magazine)
             {
                 m[i]++;
