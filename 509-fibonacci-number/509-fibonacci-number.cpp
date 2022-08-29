0ff694b6f63d27bafe1cc569e2c38107ecf0d631
Aryan-9211 <71491341+Aryan-9211@users.noreply.github.com>
2022-08-29 12:52:01 +0530

    Time: 4 ms (50.59%), Space: 5.9 MB (39.60%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    int fibo(int n, vector<int> &dp){
-        if(n == 0 || n == 1) return n;
-        if(dp[n] != -1) return dp[n];
-        return fibo(n - 1,dp) + fibo(n - 2,dp);
-    }
-    
     int fib(int n) {
-        vector<int> dp(n + 1, -1);
-        return fibo(n, dp);
+        if(n < 2) return n;
+        int a = 0, b = 1, c;
+        for(int i = 2; i <= n; i++){
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
     }
 };
