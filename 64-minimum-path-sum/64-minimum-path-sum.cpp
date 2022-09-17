a40aa13977a29866dac71047ee0d55fa6e7ffe36
Aryan-9211 <71491341+Aryan-9211@users.noreply.github.com>
2022-09-17 17:22:30 +0530

    Time: 15 ms (59.27%), Space: 10.2 MB (31.06%) - LeetHub

---
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int cost(vector<vector<int>> &grid,int i, int j, vector<vector<int>> &dp){
+        if(i == 0 && j == 0) return grid[0][0];
+        if(i < 0 || j < 0) return INT_MAX;
+        if(dp[i][j] != -1) return dp[i][j];
+        dp[i][j] = grid[i][j] + min(cost(grid, i - 1, j, dp), cost(grid, i, j - 1, dp));
+        return dp[i][j];
+    }
+    
+    int minPathSum(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        return cost(grid, n - 1, m - 1, dp);
+    }
+};
