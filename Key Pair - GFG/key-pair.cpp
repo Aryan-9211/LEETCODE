7a42818632a2d1f692456e37225d981df3edd6b7
Aryan-9211 <71491341+Aryan-9211@users.noreply.github.com>
2022-11-06 23:50:32 +0530

    Added solution - LeetHub

--- Key Pair - GFG/key-pair.cpp
+++ Key Pair - GFG/key-pair.cpp
@@ -12,20 +12,10 @@ public:
 	// whose sum is equal to the given value
 	bool hasArrayTwoCandidates(int arr[], int n, int x) {
 	    // code here
-	    vector<int> freq(1e5 + 10, 0);
+	    unordered_set<int> st;
 	    for(int i = 0; i < n; i++){
-	        freq[arr[i]]++;
-	    }
-	    
-	    for(int i = 0; i < n; i++){
-	        int diff = x - arr[i];
-	        if(diff < 0) continue;
-	        if(diff == arr[i]) {
-	            if(freq[diff] >= 2) return true;
-	        }
-	        else {
-	            if(freq[diff]  > 0) return true;
-	        }
+	        if(st.find(x - arr[i]) != st.end()) return true;
+	        st.insert(arr[i]);
 	    }
 	    return false;
 	}
