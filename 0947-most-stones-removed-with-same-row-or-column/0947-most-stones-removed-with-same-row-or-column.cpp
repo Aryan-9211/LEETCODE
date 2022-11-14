ae42a610a3d10b7e6a368aa3f5bb54d48838b3d1
Aryan-9211 <71491341+Aryan-9211@users.noreply.github.com>
2022-11-15 02:31:25 +0530

    Time: 108 ms (73.59%), Space: 19.8 MB (39.00%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,25 +1,54 @@
-class Solution {
-public:
-    int dfs(int i, int n, vector<vector<int>>& stones, vector<int>& vis){
-        vis[i] = true;
-        int ans = 0;
-        for(int j = 0; j < n; j++){
-            if(!vis[j] && (stones[j][0] == stones[i][0] || stones[j][1] == stones[i][1])){
-                ans += dfs(j, n, stones, vis);
-            }
-        }
-        return ans + 1;
+class Solution
+{
+    public:
+    int siz[100010];
+    int parent[100010];
+
+    void make(int v)
+    {
+        parent[v] = v;
+        siz[v] = 1;
     }
-    
-    int removeStones(vector<vector<int>>& stones) {
-        int ans = 0;
-        int n = stones.size();
-        vector<int> vis(n + 1, 0);
-        for(int i = 0; i < n; i++){
-             if(!vis[i]) {
-                 ans += dfs(i, n, stones, vis) - 1;
-             }
+
+    int find(int v)
+    {
+        if (v == parent[v]) return v;
+        return parent[v] = find(parent[v]);
+    }
+
+    void Union(int a, int b)
+    {
+        a = find(a);
+        b = find(b);
+        if (a != b)
+        {
+            if (siz[a] < siz[b]) swap(a, b);
+            parent[b] = a;
+            siz[a] += siz[b];
         }
-        return (ans);
+    }
+
+    int removeStones(vector<vector<int>> &stones)
+    {
+        int n = stones.size();
+        int rm = INT_MIN;
+        int cm = INT_MIN;
+        for(auto i : stones){
+            rm = max(rm, i[0]);
+            cm = max(cm, i[1]);
+        }
+        for(int i = 0; i < n; i++) stones[i][1] += (rm + 1);
+        for(int i = 0; i <= (rm + rm + cm + 1); i++) make(i);
+        set<int> st;
+        for(auto i : stones) {
+            Union(i[0], i[1]);
+            st.insert(i[0]);
+            st.insert(i[1]);
+        }
+        int ans = 0;
+        for(auto i : st){
+            if(find(i) == i) ans++;
+        }
+        return n - ans;       
     }
 };
