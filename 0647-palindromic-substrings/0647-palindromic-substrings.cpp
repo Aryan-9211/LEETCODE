10dac3c8da63df1b340df7c3d1a120dcbe289d29
Aryan-9211 <71491341+Aryan-9211@users.noreply.github.com>
2022-11-14 14:49:28 +0530

    Time: 2444 ms (5.03%), Space: 423.6 MB (7.07%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
+    int ispali(string s, int i, int j, vector<vector<int>>& dp){
+        if(i <= j) return 1;
+        if(dp[i][j] != -1) return dp[i][j];
+        return dp[i][j] = (s[i] == s[j]) ? ispali(s, i - 1, j + 1, dp) : 0;
+    }
+    
     int countSubstrings(string s) {
         int n = s.size();
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
-        //dp[i][j] = 1 if(substring from i to j is palindrome)
-        //dp[i][j] = 0 if(substring from i to j is not palindrome)
-        int ans = 0; // stores count of palindromic substrings
-        for(int i = 0; i < n; i++){
+        int ans = 0;
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        for(int i= 0; i < n; i++){
             for(int j = i; j >= 0; j--){
-                if(i == j){
-                    dp[i][j] = 1;
-                }
-                else if(j + 1 == i){
-                    dp[i][j] = (s[i] == s[j]) ? 1 : 0;
-                }
-                else {
-                    dp[i][j] = (s[i] == s[j]) ? dp[i - 1][j + 1] : 0;
-                }
-                ans += dp[i][j];
+                ans += ispali(s, i, j, dp);
+                // cout << i << " " << j << " " << ans << endl;
             }
         }
         return ans;
