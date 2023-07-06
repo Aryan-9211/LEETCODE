0b9d2139f9045893311679e1aeda1f43c93d8af2
Aryan-9211 <71491341+Aryan-9211@users.noreply.github.com>
2023-07-06 17:31:14 +0530

    Added solution - LeetHub

--- Minimal Cost - GFG/minimal-cost.cpp
+++ Minimal Cost - GFG/minimal-cost.cpp
@@ -6,21 +6,30 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    int f(int idx, vector<int>& height, int n, int k, vector<int>& dp){
-        if(idx <= 0) return 0;
-        if(dp[idx] != -1) return dp[idx];
+    // int f(int idx, vector<int>& height, int n, int k, vector<int>& dp){
+    //     if(idx <= 0) return 0;
+    //     if(dp[idx] != -1) return dp[idx];
         
-        int ans = INT_MAX;
-        for(int i = 1; i <= k; i++){
-            if(idx - i < 0) break;
-            ans = min(ans, f(idx - i, height, n, k, dp) + abs(height[idx] - height[idx - i]));
-        }
-        return dp[idx] = ans;
+    //     int ans = INT_MAX;
+    //     for(int i = 1; i <= k; i++){
+    //         if(idx - i < 0) break;
+    //         ans = min(ans, f(idx - i, height, n, k, dp) + abs(height[idx] - height[idx - i]));
+    //     }
+    //     return dp[idx] = ans;
         
-    }
+    // }
     int minimizeCost(vector<int>& height, int n, int k) {
-        vector<int> dp(n, -1);
-        return f(n - 1, height, n, k, dp);
+        vector<int> dp(n, INT_MAX);
+        dp[0] = 0;
+        for(int i = 1; i < n; i++){
+            int ans = INT_MAX;
+            for(int j = 1; j <= k; j++){
+                if(i - j < 0) break;
+                ans = min(ans, dp[i - j] + abs(height[i] - height[i - j])); 
+            }
+            dp[i] = ans;
+        }
+        return dp[n - 1];
     }
 };
 
